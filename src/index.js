a333f014626be6c42bdb15ca228d5e5b13f900c9
Nilaxi <nilaximakwana@gmail.com>
2023-11-22 16:25:55 +0530

    task2

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('main'));
 root.render(
   <React.StrictMode>
-    <Home/>
+    <Counter/>
   </React.StrictMode>
 );
 
