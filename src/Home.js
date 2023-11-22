c933eea0ff79989a7b6f67815627129b08107b77
Nilaxi <nilaximakwana@gmail.com>
2023-11-22 14:40:02 +0530

    change

--- src/Home.js
+++ src/Home.js
@@ -24,35 +24,22 @@ const Home = () => {
 
      {data2}
      <button onClick={multiply}>Multiply</button>
-
-    
-    
-
-
-    <table>
-        
-        <tbody>
+<table>
+    <tbody>
         {
-        
-            t.map((v,index)=>{
-
-                return (<tr key={index}>
-                     <td>{v.albumId}</td>
-                    <td>{v.id}</td>
-                    <td><img width={'50px'} src  = {v.url}></img></td>
-                    <td><img width={'50px'} src  = {v.thumbnailUrl}></img></td>
-                    <td>{v.title}</td>
-                    
-                    </tr>)
-
-            })
-        
+        t.map((v,index)=>{
+            return (<tr key={index}>
+                <td>{v.albumId}</td>
+                <td>{v.id}</td>
+                <td><img width={'50px'} src  = {v.url}></img></td>
+                <td><img width={'50px'} src  = {v.thumbnailUrl}></img></td>
+                <td>{v.title}</td>
+                </tr>)
+                })
         }
-        
-        </tbody>
-        </table>
-
-        </>
-  )
+    </tbody>
+</table>
+</>
+)
 }
 export default Home;
